a0770163c247191c295cb38d89229687e4cbf23f
hide2222 <abe.edipa@gmail.com>
2020-09-10 19:02:43 +0900

    selected_controller修正

--- app/controllers/selected_users_controller.rb
+++ app/controllers/selected_users_controller.rb
@@ -7,4 +7,20 @@ class SelectedUsersController < ApplicationController
   def index
     @selected_users = Selecteduser.all
   end
+
+  def create
+    @selected_user = Selecteduser.new(serected_user_params)
+    if @selected_user.save
+    redirect_to root_path
+    else
+    render :index
+    end
+  end
+
+
+  private
+
+  def serected_user_params
+    params.require(:selecteduser).merge(user_id:current_user.id,logic_result_id:logic_result_id)
+  end
 end
